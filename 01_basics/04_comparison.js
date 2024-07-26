fac2224a2a56082c2989a7abe0e918cbc9c39837
HamzaKhalid2001 <humza.kac@gmail.com>
2024-07-26 19:04:38 +0500

    data type dataTypeSummary

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,11 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+/* equality check and comparison operators works differently. Comparison operator convert "null" to a number, treating it as 0.That's why 3) is true and 1) is false*/
+
+// console.log("2" === 2)
+// strict check also check for the dataType
---
+++ 01_basics/dataTypeSummary.js
@@ -0,0 +1,17 @@
+// const id = Symbol('123');
+// const anotherId = Symbol('123');
+// console.log(id, anotherId)
+
+const bigNumber = 98789017362812182n
+
+/*because the variables are different, their symbol are also treated as different. Same as, a same sticker placed on 2 different toys can't be considered as same */
+
+// const myArr = ["Hamza", "Khalid", "Ahmed"];
+
+const myObj = {
+    name: "Hamza",
+    age: 22,
+    city: "Karachi",    
+}
+
+console.log(typeof bigNumber)
